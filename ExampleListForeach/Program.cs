aa1f38c7e0ed14a47993c0398e3e2dcd127336cc
Damian <DamianMorozov@users.noreply.github.com>
2018-08-02 17:44:54 +0300

    Initial commit

--- ExampleListForeach/Program.cs
+++ ExampleListForeach/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExampleListForeach
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var rand = new Random();
+            List<object> id = new List<object>();
+            for (var i = 1; i < 6; i++)
+                id.Add(rand.Next(0,1000).ToString().GetHashCode());
+
+            Console.WriteLine(@"Print method.");
+            id.ForEach(Print);
+            Console.WriteLine();
+
+            Console.WriteLine(@"Anonymous method.");
+            id.ForEach(item => 
+            {
+                Console.WriteLine(@"id.GetHashCode() == " + Convert.ToString(item.GetHashCode()));
+            });
+            Console.WriteLine();
+
+            Console.WriteLine(@"Anonymous method.");
+            id.ForEach(delegate (object item)
+            {
+                Console.WriteLine(@"id.GetHashCode() == " + Convert.ToString(item.GetHashCode()));
+            });
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        private static void Print(object item)
+        {
+            Console.WriteLine(@"id.GetHashCode() == " + Convert.ToString(item.GetHashCode()));
         }
     }
 }
